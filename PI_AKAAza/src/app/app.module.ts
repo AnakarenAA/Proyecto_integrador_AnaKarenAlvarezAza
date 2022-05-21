51f7f32d01fde13a3791a0d298e2b3d3885b126f
AnakarenAA <104323603+AnakarenAA@users.noreply.github.com>
2022-05-21 20:36:16 -0300

    fixed compoNETS
    solucionado problema de tipeo en carpeta components

--- PI_AKAAza/src/app/app.module.ts
+++ PI_AKAAza/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './componets/header/header.component';
-import { LogoAPComponent } from './componets/logo-ap/logo-ap.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoAPComponent
+    LogoAPComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
