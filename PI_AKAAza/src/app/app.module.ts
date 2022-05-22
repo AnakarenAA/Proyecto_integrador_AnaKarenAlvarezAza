1744dda4221c272a4eb8c34f2022612c30c419fe
AnakarenAA <104323603+AnakarenAA@users.noreply.github.com>
2022-05-22 17:17:19 -0300

    Experiencia laboral

--- PI_AKAAza/src/app/app.module.ts
+++ PI_AKAAza/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     LogoAPComponent,
     SocialComponent,
     BannerComponent,
-    AcercaDeComponent
+    AcercaDeComponent,
+    ExperienciaComponent
   ],
   imports: [
     BrowserModule,
