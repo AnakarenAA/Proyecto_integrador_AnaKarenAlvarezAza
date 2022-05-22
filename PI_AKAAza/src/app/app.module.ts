9934f3a9265b21a17838ba627d05383ea4c9c097
AnakarenAA <104323603+AnakarenAA@users.noreply.github.com>
2022-05-21 23:01:05 -0300

    FIX Bootstrap + Header retoque
    Header ahora corre en bootstrap, cuenta con nav.

--- PI_AKAAza/src/app/app.module.ts
+++ PI_AKAAza/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
