06612815e5ff73ccd203e51eba2541a8eff8f06a
AnakarenAA <104323603+AnakarenAA@users.noreply.github.com>
2022-05-25 20:43:13 -0300

    back end actualizado

---
+++ back-end/src/main/java/com/proyectointegrador/AKAAZA/AkaazaApplication.java
@@ -0,0 +1,13 @@
+package com.proyectointegrador.AKAAZA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkaazaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkaazaApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/proyectointegrador/AKAAZA/AkaazaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectointegrador.AKAAZA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkaazaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
